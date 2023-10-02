b97efe2600c8c4aafc485016fafd04fa240876eb
MUmairqureshi <67610498+MUmairqureshi@users.noreply.github.com>
2023-10-02 03:26:44 -0700

    U

--- components/Home/saleSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SaleSection = () => {
-  return (
-    <div className="flex h-screen w-full items-center justify-center bg-pink-200">SaleSection</div>
-  );
-};
-
-export default SaleSection;
