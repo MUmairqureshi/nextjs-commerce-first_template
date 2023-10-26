820afb4894ac3668cb849774f49bc471706e7f98
Talal <talalzafar84@gmail.com>
2023-10-26 23:18:29 +0500

    integrated about page with shoipfy

--- components/About/FollowUs.tsx
+++ components/About/FollowUs.tsx
@@ -1,12 +1,12 @@
 import Link from 'next/link';
 import { AiFillInstagram, AiOutlineGooglePlus, AiOutlineTwitter } from 'react-icons/ai';
 import { BiLogoFacebook } from 'react-icons/bi';
-const FollowUs = () => {
+const FollowUs = ({ title }: { title: any }) => {
   return (
     <div className="bg-trasparent flex items-center justify-center p-10 md:p-0">
       <div className="flex h-[300px] flex-col items-center justify-center gap-6 bg-background p-10 md:h-[460px] md:p-20">
         <div className="h-1 w-14 bg-primary md:w-20" />
-        <h1 className="leading-wider text-3xl font-bold tracking-wider md:text-4xl">Follow Us</h1>
+        <h1 className="leading-wider text-3xl font-bold tracking-wider md:text-4xl">{title}</h1>
         <div className="mt-4 flex gap-3 sm:gap-8 md:gap-12">
           <Link href="#">
             <BiLogoFacebook size="26" />
